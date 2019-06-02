90e8829915314f621009ec07a0e8d2c52e0e86f1
Lauren Eggers <neyabenz@gmail.com>
2019-06-02 11:06:13 -0400

    modified data for random color. Added Howl

--- js/keydata.js
+++ js/keydata.js
@@ -1,1 +1,132 @@
-var keysData = {};
+var keysData = {
+	q: {
+		sound: new Howl({
+  		urls: ['sounds/bubbles.mp3']
+		}),
+	},
+	w: {
+		sound: new Howl({
+  		urls: ['sounds/clay.mp3']
+		}),
+	},
+	e: {
+		sound: new Howl({
+  		urls: ['sounds/confetti.mp3']
+		}),
+	},
+	r: {
+		sound: new Howl({
+  		urls: ['sounds/corona.mp3']
+		}),
+	},
+		t: {
+		sound: new Howl({
+  		urls: ['sounds/dotted-spiral.mp3']
+		}),
+	},
+	y: {
+		sound: new Howl({
+  		urls: ['sounds/flash-1.mp3']
+		}),
+	},
+	u: {
+		sound: new Howl({
+  		urls: ['sounds/flash-2.mp3']
+		}),
+	},
+	i: {
+		sound: new Howl({
+  		urls: ['sounds/flash-3.mp3']
+		}),
+	},
+	o: {
+		sound: new Howl({
+			urls: ['sounds/glimmer.mp3']
+		}),
+	},
+	p: {
+		sound: new Howl({
+  		urls: ['sounds/moon.mp3']
+		}),
+	},
+	a: {
+		sound: new Howl({
+  		urls: ['sounds/pinwheel.mp3']
+		}),
+	},
+	s: {
+		sound: new Howl({
+  		urls: ['sounds/piston-1.mp3']
+		}),
+	},
+		d: {
+		sound: new Howl({
+  		urls: ['sounds/piston-2.mp3']
+		}),
+	},
+	f: {
+		sound: new Howl({
+  		urls: ['sounds/prism-1.mp3']
+		}),
+	},
+	g: {
+		sound: new Howl({
+  		urls: ['sounds/prism-2.mp3']
+		}),
+	},
+	h: {
+		sound: new Howl({
+  		urls: ['sounds/prism-3.mp3']
+		}),
+	},
+	j: {
+		sound: new Howl({
+  		urls: ['sounds/splits.mp3']
+		}),
+	},
+	k: {
+		sound: new Howl({
+  		urls: ['sounds/squiggle.mp3']
+		}),
+	},
+	l: {
+		sound: new Howl({
+  		urls: ['sounds/strike.mp3']
+		}),
+	},
+	z: {
+		sound: new Howl({
+  		urls: ['sounds/suspension.mp3']
+		}),
+	},
+	x: {
+		sound: new Howl({
+  		urls: ['sounds/timer.mp3']
+		}),
+	},
+	c: {
+		sound: new Howl({
+  		urls: ['sounds/ufo.mp3']
+		}),
+	},
+	v: {
+		sound: new Howl({
+  		urls: ['sounds/veil.mp3']
+		}),
+	},
+	b: {
+		sound: new Howl({
+  		urls: ['sounds/wipe.mp3']
+		}),
+	},
+	n: {
+		sound: new Howl({
+			urls: ['sounds/zig-zag.mp3']
+		}),
+	},
+	m: {
+		sound: new Howl({
+  		urls: ['sounds/moon.mp3']
+		}),
+	}
+}
