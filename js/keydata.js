4c24b0a0327a4b0374db76c6ef3733e389dc0658
Lauren Eggers <neyabenz@gmail.com>
2019-06-02 11:29:44 -0400

    fixed wrong directory of sounds

--- js/keydata.js
+++ js/keydata.js
@@ -1,132 +1,132 @@
 var keysData = {
 	q: {
 		sound: new Howl({
-  		urls: ['sounds/bubbles.mp3']
+  		src: ['sounds/bubbles.mp3']
 		}),
 	},
 	w: {
 		sound: new Howl({
-  		urls: ['sounds/clay.mp3']
+  		src: ['sounds/clay.mp3']
 		}),
 	},
 	e: {
 		sound: new Howl({
-  		urls: ['sounds/confetti.mp3']
+  		src: ['sounds/confetti.mp3']
 		}),
 	},
 	r: {
 		sound: new Howl({
-  		urls: ['sounds/corona.mp3']
+  		src: ['sounds/corona.mp3']
 		}),
 	},
 		t: {
 		sound: new Howl({
-  		urls: ['sounds/dotted-spiral.mp3']
+  		src: ['sounds/dotted-spiral.mp3']
 		}),
 	},
 	y: {
 		sound: new Howl({
-  		urls: ['sounds/flash-1.mp3']
+  		src: ['sounds/flash-1.mp3']
 		}),
 	},
 	u: {
 		sound: new Howl({
-  		urls: ['sounds/flash-2.mp3']
+  		src: ['sounds/flash-2.mp3']
 		}),
 	},
 	i: {
 		sound: new Howl({
-  		urls: ['sounds/flash-3.mp3']
+  		src: ['sounds/flash-3.mp3']
 		}),
 	},
 	o: {
 		sound: new Howl({
-			urls: ['sounds/glimmer.mp3']
+			src: ['sounds/glimmer.mp3']
 		}),
 	},
 	p: {
 		sound: new Howl({
-  		urls: ['sounds/moon.mp3']
+  		src: ['sounds/moon.mp3']
 		}),
 	},
 	a: {
 		sound: new Howl({
-  		urls: ['sounds/pinwheel.mp3']
+  		src: ['sounds/pinwheel.mp3']
 		}),
 	},
 	s: {
 		sound: new Howl({
-  		urls: ['sounds/piston-1.mp3']
+  		src: ['sounds/piston-1.mp3']
 		}),
 	},
 		d: {
 		sound: new Howl({
-  		urls: ['sounds/piston-2.mp3']
+  		src: ['sounds/piston-2.mp3']
 		}),
 	},
 	f: {
 		sound: new Howl({
-  		urls: ['sounds/prism-1.mp3']
+  		src: ['sounds/prism-1.mp3']
 		}),
 	},
 	g: {
 		sound: new Howl({
-  		urls: ['sounds/prism-2.mp3']
+  		src: ['sounds/prism-2.mp3']
 		}),
 	},
 	h: {
 		sound: new Howl({
-  		urls: ['sounds/prism-3.mp3']
+  		src: ['sounds/prism-3.mp3']
 		}),
 	},
 	j: {
 		sound: new Howl({
-  		urls: ['sounds/splits.mp3']
+  		src: ['sounds/splits.mp3']
 		}),
 	},
 	k: {
 		sound: new Howl({
-  		urls: ['sounds/squiggle.mp3']
+  		src: ['sounds/squiggle.mp3']
 		}),
 	},
 	l: {
 		sound: new Howl({
-  		urls: ['sounds/strike.mp3']
+  		src: ['sounds/strike.mp3']
 		}),
 	},
 	z: {
 		sound: new Howl({
-  		urls: ['sounds/suspension.mp3']
+  		src: ['sounds/suspension.mp3']
 		}),
 	},
 	x: {
 		sound: new Howl({
-  		urls: ['sounds/timer.mp3']
+  		src: ['sounds/timer.mp3']
 		}),
 	},
 	c: {
 		sound: new Howl({
-  		urls: ['sounds/ufo.mp3']
+  		src: ['sounds/ufo.mp3']
 		}),
 	},
 	v: {
 		sound: new Howl({
-  		urls: ['sounds/veil.mp3']
+  		src: ['sounds/veil.mp3']
 		}),
 	},
 	b: {
 		sound: new Howl({
-  		urls: ['sounds/wipe.mp3']
+  		src: ['sounds/wipe.mp3']
 		}),
 	},
 	n: {
 		sound: new Howl({
-			urls: ['sounds/zig-zag.mp3']
+			src: ['sounds/zig-zag.mp3']
 		}),
 	},
 	m: {
 		sound: new Howl({
-  		urls: ['sounds/moon.mp3']
+  		src: ['sounds/moon.mp3']
 		}),
 	}
 }
